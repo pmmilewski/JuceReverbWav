f04896f3921dd8803dab549b35243e221a6b89de
pmmilewski <milewski.piotr.m@gmail.com>
2018-06-17 14:53:50 +0200

    More on less it works with my reverbs, start to create needed interface

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -10,50 +10,63 @@
 
 //==============================================================================
 MainComponent::MainComponent()
+    : state(Stopped)
 {
-    // Make sure you set the size of the component after
-    // you add any child components.
-    setSize (800, 600);
+    addAndMakeVisible(&openButton);
+    openButton.setButtonText("Open...");
+    openButton.onClick = [this] { openButtonClicked(); };
 
-    // specify the number of input and output channels that we want to open
-    setAudioChannels (2, 2);
+    addAndMakeVisible(&playButton);
+    playButton.setButtonText("Play");
+    playButton.onClick = [this] { playButtonClicked(); };
+    playButton.setColour(TextButton::buttonColourId, Colours::green);
+    playButton.setEnabled(false);
+
+    addAndMakeVisible(&stopButton);
+    stopButton.setButtonText("Stop");
+    stopButton.onClick = [this] { stopButtonClicked(); };
+    stopButton.setColour(TextButton::buttonColourId, Colours::red);
+    stopButton.setEnabled(false);
+
+    addAndMakeVisible(&loopingToggle);
+    loopingToggle.setButtonText("Loop");
+    loopingToggle.onClick = [this] { loopButtonChanged(); };
+
+    addAndMakeVisible(&currentPositionLabel);
+    currentPositionLabel.setText("Stopped", dontSendNotification);
+
+    setSize(600, 400);
+
+    formatManager.registerBasicFormats();
+    transportSource.addChangeListener(this);
+
+    left_allpassReverbs = std::make_unique<AllpassReverbSeries>();
+    left_combReverbs = std::make_unique<CombReverbParallel>();
+
+    right_allpassReverbs = std::make_unique<AllpassReverbSeries>();
+    right_combReverbs = std::make_unique<CombReverbParallel>(
+    );
+
+    left_allpassReverbs->addBlock(4000, 0.6);
+    left_allpassReverbs->addBlock(1500, 0.6);
+    left_allpassReverbs->addBlock(500, 0.6);
+    left_allpassReverbs->addBlock(150, 0.6);
+    left_combReverbs->addBlock(30000, 0.7);
+
+    right_allpassReverbs->addBlock(4000, 0.6);
+    right_allpassReverbs->addBlock(1500, 0.6);
+    right_allpassReverbs->addBlock(500, 0.6);
+    right_allpassReverbs->addBlock(150, 0.6);
+    right_combReverbs->addBlock(30000, 0.7);
+
+    setAudioChannels(2, 2);
+    startTimer(20);
 }
 
 MainComponent::~MainComponent()
 {
     // This shuts down the audio device and clears the audio source.
     shutdownAudio();
-}
-
-//==============================================================================
-void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
-{
-    // This function will be called when the audio device is started, or when
-    // its settings (i.e. sample rate, block size, etc) are changed.
-
-    // You can use this function to initialise any resources you might need,
-    // but be careful - it will be called on the audio thread, not the GUI thread.
-
-    // For more details, see the help for AudioProcessor::prepareToPlay()
-}
-
-void MainComponent::getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill)
-{
-    // Your audio-processing code goes here!
-
-    // For more details, see the help for AudioProcessor::getNextAudioBlock()
-
-    // Right now we are not producing any data, in which case we need to clear the buffer
-    // (to prevent the output of random noise)
-    bufferToFill.clearActiveBufferRegion();
-}
-
-void MainComponent::releaseResources()
-{
-    // This will be called when the audio device stops, or when it is being
-    // restarted due to a setting change.
-
-    // For more details, see the help for AudioProcessor::releaseResources()
 }
 
 //==============================================================================
@@ -70,4 +83,148 @@ void MainComponent::resized()
     // This is called when the MainContentComponent is resized.
     // If you add any child components, this is where you should
     // update their positions.
+    openButton.setBounds(10, 10, getWidth() - 20, 20);
+    playButton.setBounds(10, 40, getWidth() - 20, 20);
+    stopButton.setBounds(10, 70, getWidth() - 20, 20);
+    loopingToggle.setBounds(10, 100, getWidth() - 20, 20);
+    currentPositionLabel.setBounds(10, 130, getWidth() - 20, 20);
+}
+
+void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
+{
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void MainComponent::getNextAudioBlock(const AudioSourceChannelInfo &bufferToFill)
+{
+    if (readerSource.get() == nullptr)
+    {
+        bufferToFill.clearActiveBufferRegion();
+        return;
+    }
+
+    transportSource.getNextAudioBlock(bufferToFill);
+    
+    if(state == TransportState::Playing)
+    {
+        float* leftChannelPtr = bufferToFill.buffer->getWritePointer(0);
+        float* rightChannelPtr = bufferToFill.buffer->getWritePointer(1);
+        int numberOfSamples = bufferToFill.numSamples;
+        
+        for(size_t i = 0; i < numberOfSamples; i++)
+        {
+            *(leftChannelPtr+i) += left_allpassReverbs->process(*(leftChannelPtr+i));
+            *(rightChannelPtr+i) += right_allpassReverbs->process(*(rightChannelPtr+i));
+        }
+    }
+    
+}
+
+void MainComponent::releaseResources()
+{
+    transportSource.releaseResources();
+}
+
+void MainComponent::changeListenerCallback(ChangeBroadcaster *source)
+{
+    if (source == &transportSource)
+    {
+        if (transportSource.isPlaying())
+            changeState(Playing);
+        else
+            changeState(Stopped);
+    }
+}
+
+void MainComponent::timerCallback()
+{
+    if (transportSource.isPlaying())
+    {
+        RelativeTime position(transportSource.getCurrentPosition());
+
+        auto minutes = ((int)position.inMinutes()) % 60;
+        auto seconds = ((int)position.inSeconds()) % 60;
+        auto millis = ((int)position.inMilliseconds()) % 1000;
+
+        auto positionString = String::formatted("%02d:%02d:%03d", minutes, seconds, millis);
+
+        currentPositionLabel.setText(positionString, dontSendNotification);
+    }
+    else
+    {
+        currentPositionLabel.setText("Stopped", dontSendNotification);
+    }
+}
+
+void MainComponent::updateLoopState(bool shouldLoop)
+{
+    if (readerSource.get() != nullptr)
+        readerSource->setLooping(shouldLoop);
+}
+
+void MainComponent::changeState(TransportState newState)
+{
+    if (state != newState)
+    {
+        state = newState;
+
+        switch (state)
+        {
+        case Stopped:
+            stopButton.setEnabled(false);
+            playButton.setEnabled(true);
+            transportSource.setPosition(0.0);
+            break;
+
+        case Starting:
+            playButton.setEnabled(false);
+            transportSource.start();
+            break;
+
+        case Playing:
+            stopButton.setEnabled(true);
+            break;
+
+        case Stopping:
+            transportSource.stop();
+            break;
+        }
+    }
+}
+
+void MainComponent::openButtonClicked()
+{
+    FileChooser chooser("Select a Wave file to play...",
+                        File::nonexistent,
+                        "*.wav");
+
+    if (chooser.browseForFileToOpen())
+    {
+        auto file = chooser.getResult();
+        auto *reader = formatManager.createReaderFor(file);
+
+        if (reader != nullptr)
+        {
+            std::unique_ptr<AudioFormatReaderSource> newSource(new AudioFormatReaderSource(reader, true));
+            transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);
+            playButton.setEnabled(true);
+            readerSource.reset(newSource.release());
+        }
+    }
+}
+
+void MainComponent::playButtonClicked()
+{
+    updateLoopState(loopingToggle.getToggleState());
+    changeState(Starting);
+}
+
+void MainComponent::stopButtonClicked()
+{
+    changeState(Stopping);
+}
+
+void MainComponent::loopButtonChanged()
+{
+    updateLoopState(loopingToggle.getToggleState());
 }
--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -9,13 +9,17 @@
 #pragma once
 
 #include "../JuceLibraryCode/JuceHeader.h"
+#include "CombReverbParallel.h"
+#include "AllpassReverbSeries.h"
 
 //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
 */
-class MainComponent   : public AudioAppComponent
+class MainComponent   : public AudioAppComponent,
+                        public ChangeListener,
+                        public Timer
 {
 public:
     //==============================================================================
@@ -23,17 +27,54 @@ public:
     ~MainComponent();
 
     //==============================================================================
-    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
-    void getNextAudioBlock (const AudioSourceChannelInfo& bufferToFill) override;
+    void prepareToPlay (int, double) override;
+    void getNextAudioBlock (const AudioSourceChannelInfo&) override;
     void releaseResources() override;
 
     //==============================================================================
     void paint (Graphics& g) override;
     void resized() override;
 
+    void changeListenerCallback (ChangeBroadcaster*) override;
+
+    void timerCallback() override;
+    void updateLoopState (bool);
+
 private:
+     enum TransportState
+    {
+        Stopped,
+        Starting,
+        Playing,
+        Stopping
+    };
+
+    void changeState (TransportState);
+    void openButtonClicked();
+    void playButtonClicked();
+    void stopButtonClicked();
+    void loopButtonChanged();
     //==============================================================================
     // Your private member variables go here...
+
+    TextButton openButton;
+    TextButton playButton;
+    TextButton stopButton;
+    ToggleButton loopingToggle;
+    Label currentPositionLabel;
+
+    AudioFormatManager formatManager;
+    std::unique_ptr<AudioFormatReaderSource> readerSource;
+
+    std::unique_ptr<CombReverbParallel> left_combReverbs;
+    std::unique_ptr<AllpassReverbSeries> left_allpassReverbs;
+
+    std::unique_ptr<CombReverbParallel> right_combReverbs;
+    std::unique_ptr<AllpassReverbSeries> right_allpassReverbs;
+
+    AudioTransportSource transportSource;
+    TransportState state;
+
 
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
